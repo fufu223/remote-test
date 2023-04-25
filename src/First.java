25f8ef109e2fd99e572948ec15b7305a3d1e0e2d
fufu223 <hsf6661@163.com>
2023-04-25 22:11:31 +0800

    init

---
+++ src/First.java
@@ -0,0 +1,10 @@
+/**
+ * @author: BlcakTiger
+ * @date: 2023/4/25 22:10
+ * @contact: hsf6661@163.com
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
